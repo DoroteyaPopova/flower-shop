f5bcbe3e2b4628c221738d43b56ba5b0428f283e
doroteya <snowsocial7@gmail.com>
2024-07-21 20:31:06 +0300

    created main components

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,201 @@
+export default function Body() {
+    return (
+        <><main id="content-box" className="order-first">
+            <div className="banner-section section parallax-window" data-parallax="scroll" data-image-src="img/section-1-bg.jpg" id="section-1">
+                <div className="container">
+                    <div className="item">
+                        <div className="bg-blue-transparent logo-fa"><span><i className="fas fa-2x fa-atom"></i></span> Simply Amazed</div>
+                        <div className="bg-blue-transparent simple"><p>Your simplest HTML template, the most amazing page ever, yet free!</p></div>
+                    </div>
+                </div>
+            </div>
+
+            <section className="work-section section" id="section-2">
+                <div className="container">
+                    <div className="row">
+                        <div className="item col-md-4">
+                            <div className="tm-work-item-inner">
+                                <div className="icn"><i className="fas fa-2x fa-icons"></i></div>
+                                <h3>.01 Get An Idea</h3>
+                                <p>Simply Amazed is free HTML template provided by Tooplate website. Please tell your friends about our website. Thank you.</p>
+                            </div>
+                        </div>
+                        <div className="item col-md-4 one">
+                            <div className="tm-work-item-inner">
+                                <div className="icn"><i className="fas fa-2x fa-tools"></i></div>
+                                <h3>.02 Create It</h3>
+                                <p>This is a full-width CSS template with bootstrap 5, you can download, modify and use this layout for your simple CSS website.</p>
+                            </div>
+                        </div>
+                        <div className="item col-md-4 two">
+                            <div className="tm-work-item-inner">
+                                <div className="icn"><i className="fab fa-2x fa-phoenix-framework"></i></div>
+                                <h3>.03 Execute it</h3>
+                                <p>You are allowed to use this for your personal and commerical projects, you are not allowed to redistribute the template ZIP file on any download site.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="title">
+                        <h2>Our Work</h2>
+                    </div>
+                </div>
+            </section>
+
+            <section className="gallery-section section parallax-window" data-parallax="scroll" data-image-src="img/section-3-bg.jpg" id="section-3">
+                <div className="container">
+                    <div className="title text-right">
+                        <h2>Our Gallery</h2>
+                    </div>
+                    <div className="mx-auto gallery-slider">
+                        <figure className="effect-julia item">
+                            <img src="img/gallery-img-01.jpg" alt="Image" />
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure className="effect-julia item">
+                            <img src="img/gallery-img-02.jpg" alt="Image" />
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure className="effect-julia item">
+                            <img src="img/gallery-img-03.jpg" alt="Image" />
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure className="effect-julia item">
+                            <img src="img/gallery-img-04.jpg" alt="Image" />
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure className="effect-julia item">
+                            <img src="img/gallery-img-05.jpg" alt="Image" />
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure className="effect-julia item">
+                            <img src="img/gallery-img-06.jpg" alt="Image" />
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure className="effect-julia item">
+                            <img src="img/gallery-img-07.jpg" alt="Image" />
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure className="effect-julia item">
+                            <img src="img/gallery-img-08.jpg" alt="Image" />
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                        <figure className="effect-julia item">
+                            <img src="img/gallery-img-09.jpg" alt="Image" />
+                            <figcaption>
+                                <div>
+                                    <p>Julia dances in the deep dark</p>
+                                </div>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </section>
+
+            <section className="contact-section section" id="section-4">
+                <div className="container">
+                    <div className="title">
+                        <h3>Contact Us</h3>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-5 col-md-6 mb-4 contact-form">
+                            <div className="form tm-contact-item-inner">
+                                <form action="#" method="POST">
+                                    <div className="form-group">
+                                        <input name="name" type="text" className="form-control" placeholder="Name" />
+                                    </div>
+                                    <div className="form-group">
+                                        <input name="email" type="text" className="form-control" placeholder="Email" />
+                                    </div>
+                                    <div className="form-group">
+                                        <textarea name="message" className="textarea form-control" placeholder="Message"></textarea>
+                                    </div>
+                                    <div className="form-group text-right">
+                                        <input type="submit" className="btn btn-primary" value="Send it" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 mb-4 contact-details">
+                            <div className="tm-contact-item-inner-2">
+                                <p>Nam mollis felis elementum, placerat dolor id, vehicula libero. Etiam dui nisl, mattis ut rhoncus et, cursus ut diam.</p>
+                                <ul className="font-weight-light">
+                                    <li>
+                                        <span className="icn"><i className="fas fa-mobile-alt"></i></span>
+                                        <span className="lbl">Tel:</span> <a href="#">010-020-0340</a>
+                                    </li>
+                                    <li>
+                                        <span className="icn"><i className="fas fa-at"></i></span>
+                                        <span className="lbl">Email:</span> <a href="#">info@company.com</a>
+                                    </li>
+                                    <li>
+                                        <span className="icn"><i className="fas fa-globe-asia"></i></span>
+                                        <span className="lbl">URL:</span> <a href="#">www.company.com</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-12 map">
+
+                            {/* <!-- Map --> */}
+                            <div className="map-outer tm-mb-40">
+                                <div className="gmap-canvas">
+                                    <iframe width="100%" height="400" id="gmap-canvas"
+                                        src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                                        frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <footer className="footer container container-2">
+                    <div className="row">
+                        <p className="col-sm-7">Copyright 2021 Simply Amazed Co.</p>
+                        <p className="col-sm-5 text-right design">Design: <a rel="nofollow" href="https://www.tooplate.com" target="_parent">Tooplate</a></p>
+                    </div>
+                </footer>
+            </section>
+        </main>
+        </>
+    )
+}
---
+++ client/src/components/Button.jsx
@@ -0,0 +1,13 @@
+export default function Button() {
+    return (
+        <>
+            <button className="navbar-button collapsed" type="button">
+                <span className="menu_icon">
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </span>
+            </button>
+        </>
+    )
+}
