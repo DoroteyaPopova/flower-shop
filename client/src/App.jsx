a55012922602bc3fe03425010eace4feecec1f95
doroteya <snowsocial7@gmail.com>
2024-07-21 19:25:19 +0300

    Project started. Added client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <>
+
+
+      <script src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery.singlePageNav.min.js"></script>
+      <script src="js/slick.js"></script>
+      <script src="js/parallax.min.js"></script>
+      <script src="js/templatemo-script.js"></script>
+    </>
+  )
+}
+
+export default App
