6fee793f9900d31bda5c1fd91766be5a69b4bab8
doroteya <snowsocial7@gmail.com>
2024-07-23 23:31:02 +0300

    fixing boxes places in the banner

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -1,4 +1,5 @@
-import styles from "./Banner.module.css"
+import styles from "./Banner.module.css";
+import Card from 'react-bootstrap/Card';
 
 export default function Banner() {
     return (
@@ -6,14 +7,20 @@ export default function Banner() {
             <div className="banner-section section parallax-window" data-parallax="scroll" data-image-src="img/section-1-bg.jpg" id="section-1">
                 <div className="container">
                     <div className="item">
-                        <div className="bg-blue-transparent logo-fa">
+                        {/* <div className="bg-blue-transparent logo-fa">
                             <span className={styles.logo}></span>
                             The Silver Flower
+                        </div> */}
+                        <div className={styles.left}>
+                            <img src="../../../img/icon-flower.jpg" alt="Flower" />
+                            <p>The Silver Flower</p>
                         </div>
+                        {/* <div className="bg-blue-transparent simple"><p>Your most trusted flower shop!</p></div> */}
                         <div className={styles.right}>
-                            <p>Hello World!</p>
+                            <p>
+                                Your most trusted flower shop!
+                            </p>
                         </div>
-                        <div className="bg-blue-transparent simple"><p>Your most trusted flower shop!</p></div>
                     </div>
                 </div>
             </div>
