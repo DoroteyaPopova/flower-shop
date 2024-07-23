a0a5c393a7e62644b3e6569177634cf16705a49a
doroteya <snowsocial7@gmail.com>
2024-07-24 00:25:36 +0300

    removed favicon's background

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -6,10 +6,6 @@ export default function Banner() {
             <div className="banner-section section parallax-window" data-parallax="scroll" data-image-src="/img/bg-flower.jpg" id="section-1">
                 <div className="container">
                     <div className="item">
-                        {/* <div className="bg-blue-transparent logo-fa">
-                            <span className={styles.logo}></span>
-                            The Silver Flower
-                        </div> */}
                         <div className={styles.left}>
                             <img src="../../../img/trans2.png" alt="Flower" />
                             <p>The Silver Flower</p>
