624f43b56199b2f3d7826c14b50378c19c2238db
doroteya <snowsocial7@gmail.com>
2024-07-28 17:54:50 +0300

    Added ScrollTo functionality to some navigation elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, ScrollRestoration } from "react-router-dom";
+
+
 import Home from "./components/home/Home"
+import Footer from "./components/footer/Footer";
+import Navigation from "./components/navigation/Navigation";
+import Banner from "./components/banner/Banner";
+import Info from "./components/info/Info";
+import Catalog from "./components/catalog/Catalog";
+import Button from "./components/Button";
+import ScrollToPlace from "./components/ScrollToPlace";
 
 function App() {
 
   return (
     <>
-
+      <ScrollToPlace />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/logour" element={<Logout />} />
-        <Route path="/games" element={<GameList />} />
-        <Route path="/games/create" element={<GameCrete />} /> */}
+        {/* <Route path="/" element={<Navigation />} />
+        <Route path="/" element={<Banner />} />
+        <Route path="/" element={<Info />} />
+        <Route path="/" element={<Catalog />} />
+        <Route path="/" element={<Footer />} /> */}
+        {/* <Route path="/games/create" element={<Button />} /> */}
       </Routes>
     </>
   )
--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from "./Navigations.module.css"
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 
 import Info from "../info/Info";
 
@@ -11,12 +11,36 @@ function Navigation() {
   return (
     <Navbar className={styles.navMain} fixed='top'>
       <Container>
-        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Brand href="/"
+          onClick={() => {
+            const element = document.getElementById('section-1');
+            element?.scrollIntoView({
+              behavior: 'smooth'
+            });
+          }}
+        >Home</Navbar.Brand>
         <Navbar className={styles.muted}>
-          <Nav.Link href="#home" className='text-muted'>Flowers</Nav.Link>
-          <Nav.Link href="#link" className='text-muted'>Register</Nav.Link>
-          <Nav.Link href="#link" className='text-muted'>Login</Nav.Link>
-          <Nav.Link href="/footer" className='text-muted'>About Us</Nav.Link>
+          <Nav.Link href="#home"
+            onClick={() => {
+              const element = document.getElementById('section-3');
+              element?.scrollIntoView({
+                behavior: 'smooth'
+              });
+            }}
+            className='text-muted'
+          >Flowers</Nav.Link>
+          <Nav.Link href="#home" className='text-muted'>Register</Nav.Link>
+          <Nav.Link href="#home" className='text-muted'>Login</Nav.Link>
+          <Nav.Link
+            href="#home"
+            onClick={() => {
+              const element = document.getElementById('section-4');
+              element?.scrollIntoView({
+                behavior: 'smooth'
+              });
+            }}
+            className='text-muted'
+          >AboutUs</Nav.Link>
 
           {/* Loged in */}
           {/* <Nav.Link href="#link" className='text-muted'>Add Product</Nav.Link>
