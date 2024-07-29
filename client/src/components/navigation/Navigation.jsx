25ca798e89125dc71d433e5c1bad34f8ea2a816c
doroteya <snowsocial7@gmail.com>
2024-07-29 23:34:49 +0300

    cleaned navigation's code

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -2,7 +2,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from "./Navigations.module.css"
-import { Routes, Route, } from "react-router-dom";
 
 import Info from "../info/Info";
 
@@ -20,7 +19,7 @@ function Navigation() {
           <Nav.Link href="#home" className='text-muted'>Login</Nav.Link>
 
           {/* Loged in */}
-          {/* <Nav.Link href="#link" className='text-muted'>Add Product</Nav.Link>
+          {/* <Nav.Link href="#link" className='text-muted' data-scroll-goto="1">Add Product</Nav.Link>
           <Nav.Link href="#link" className='text-muted'>Logout</Nav.Link> */}
         </Navbar>
         <Navbar className="justify-content-end">
