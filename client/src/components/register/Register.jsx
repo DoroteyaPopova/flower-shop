c7199f667555b22c9c6c3eceba549416dacfa2ab
doroteya <snowsocial7@gmail.com>
2024-08-04 12:52:32 +0300

    implemented basic register form

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,80 +1,135 @@
-import { useNavigate } from "react-router-dom";
-import { useRegister } from "../../hooks/useAuth"
-import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useRegister } from "../../hooks/useAuth"
+// import { useState } from "react";
 
-const initialValues = { email: ``, password: ``, rePassword: `` };
+// const initialValues = { email: ``, password: ``, rePassword: `` };
+
+// export default function Register() {
+//     const [error, setError] = useState(``);
+//     const register = useRegister();
+//     const navigate = useNavigate();
+
+//     const registerHandler = async ({ email, password, rePassword }) => {
+//         if (password !== rePassword) {
+//             return setError(`Password missmatch!`);
+//         }
+
+//         try {
+//             await register(email, password);
+//             navigate(`/`);
+//         } catch (err) {
+//             setError(err.message);
+//         }
+//     }
+
+//     const {
+//         values,
+//         changeHandler,
+//         submitHandler,
+//     } = useForm(initialValues, registerHandler)
+
+//     return (
+//         <>
+//             <section id="register-page" class="content auth" />
+//             <form id="register" onSubmit={submitHandler} />
+//             <div class="container">
+//                 <div class="brand-logo"></div>
+//                 <h1>Register</h1>
+
+//                 <label for="email">Email:</label>
+//                 <input
+//                     type="email"
+//                     id="email"
+//                     value={values.email}
+//                     onChange={changeHandler}
+//                     name="email"
+//                     placeholder="maria@email.com"
+//                 />
+
+//                 <label for="pass">Password:</label>
+//                 <input
+//                     type="password"
+//                     name="password"
+//                     value={values.password}
+//                     onChange={changeHandler}
+//                     id="register-password"
+//                 />
+
+//                 <label for="con-pass">Confirm Password:</label>
+//                 <input
+//                     type="password"
+//                     name="rePassword"
+//                     value={values.rePassword}
+//                     onChange={changeHandler}
+//                     id="confirm-password"
+//                 />
+//                 {error && (
+//                     <p>
+//                         <span>{error}</span>
+//                     </p>
+//                 )}
+
+//                 <input class="btn submit" type="submit" value="Register" />
+
+//                 <p class="field">
+//                     <span>If you already have profile click <a href="/login">here</a></span>
+//                 </p>
+//             </div>
+//         </>
+//     )
+// }
 
 export default function Register() {
-    const [error, setError] = useState(``);
-    const register = useRegister();
-    const navigate = useNavigate();
-
-    const registerHandler = async ({ email, password, rePassword }) => {
-        if (password !== rePassword) {
-            return setError(`Password missmatch!`);
-        }
-
-        try {
-            await register(email, password);
-            navigate(`/`);
-        } catch (err) {
-            setError(err.message);
-        }
-    }
-
-    const {
-        values,
-        changeHandler,
-        submitHandler,
-    } = useForm(initialValues, registerHandler)
-
     return (
         <>
-            <section id="register-page" class="content auth" />
-            <form id="register" onSubmit={submitHandler} />
-            <div class="container">
-                <div class="brand-logo"></div>
-                <h1>Register</h1>
+            <form id="form" action="/submit" method="post" data-scroll-index="3">
+                <div>
 
-                <label for="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    value={values.email}
-                    onChange={changeHandler}
-                    name="email"
-                    placeholder="maria@email.com"
-                />
+                    <span >
+                        <h1>Register</h1>
 
-                <label for="pass">Password:</label>
-                <input
-                    type="password"
-                    name="password"
-                    value={values.password}
-                    onChange={changeHandler}
-                    id="register-password"
-                />
+                        <label for="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value=""
+                            onChange=""
+                            name="email"
+                            placeholder="maria@email.com"
+                        />
 
-                <label for="con-pass">Confirm Password:</label>
-                <input
-                    type="password"
-                    name="rePassword"
-                    value={values.rePassword}
-                    onChange={changeHandler}
-                    id="confirm-password"
-                />
-                {error && (
-                    <p>
-                        <span>{error}</span>
-                    </p>
-                )}
+                        <label for="pass">Password:</label>
+                        <input
+                            type="password"
+                            name="password"
+                            value=""
+                            onChange=""
+                            id="register-password"
+                        />
 
-                <input class="btn submit" type="submit" value="Register" />
+                        <label for="con-pass">Confirm Password:</label>
+                        <input
+                            type="password"
+                            name="rePassword"
+                            value=""
+                            onChange=""
+                            id="confirm-password"
+                        />
+                        {/* {error && ( */}
+                            <p>
+                                <span>{}</span>
+                            </p>
+                        {/* )} */}
 
-                <p class="field">
-                    <span>If you already have profile click <a href="/login">here</a></span>
-                </p>
-            </div>
+                        <input className="btn submit" type="submit" value="Register" />
+
+                        <p className="field">
+                            <span>If you already have profile click <a href="/login">here</a></span>
+                        </p>
+                    </span>
+                </div>
+            </form>
+
         </>
     )
 }
