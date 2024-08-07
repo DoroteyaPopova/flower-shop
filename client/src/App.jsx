ba3756a123da5864b1ced170f0f13f2ea14b27b8
doroteya <snowsocial7@gmail.com>
2024-08-07 23:47:25 +0300

    added authContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,26 +8,44 @@ import LoginNew from "./components/login/LoginNew";
 import RegisterNew from "./components/register/RegisterNew";
 import CreateFlowerNew from "./components/create-flower/CreateFlowerNew";
 import DetailsFlower from "./components/details-flower/DetailsFlower";
+import { useState } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+
+  const [authState, setAuthState] = useState({});
+
+  const changeAuthState = (state) => {
+    setAuthState(state)
+  }
+
+  const contextData = {
+    userId: authState._id,
+    email: authState.email,
+    accessToken: authState.accessToken,
+    isAuthenticated: !!authState.email,
+    changeAuthState,
+  }
 
   return (
     <>
       <ScrollToPlace />
 
-      <Navigation />
+      <AuthContext.Provider value={contextData}>
+        <Navigation />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        <Route path="/register" element={<RegisterNew />} />
+          <Route path="/register" element={<RegisterNew />} />
 
-        <Route path="/login" element={<LoginNew />} />
+          <Route path="/login" element={<LoginNew />} />
 
-        <Route path="/create" element={<CreateFlowerNew />} />
+          <Route path="/create" element={<CreateFlowerNew />} />
 
-        <Route path="/flowers/:flowerId/description" element={<DetailsFlower />} />
-      </Routes>
+          <Route path="/flowers/:flowerId/description" element={<DetailsFlower />} />
+        </Routes>
+      </AuthContext.Provider>
     </>
   )
 }
