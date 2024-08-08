cd4965521e7e1391c29a565d142d4ab13bed5bcf
doroteya <snowsocial7@gmail.com>
2024-08-09 00:03:44 +0300

    fixed navigation display

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -7,29 +7,41 @@ import { Link } from 'react-router-dom';
 import styles from "./Navigations.module.css"
 
 import Info from "../info/Info";
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 
 function Navigation() {
+
+  const { isAuthenticated, email } = useContext(AuthContext);
 
   return (
     <Navbar className={styles.navMain} fixed='top'>
       <Container>
         <Navbar.Brand as={Link} to="/" data-scroll-goto="0">Home</Navbar.Brand>
-        <Navbar className={styles.muted}>
-          <Nav.Link as={Link} to="/" className='text-muted' data-scroll-goto="2">Flowers</Nav.Link>
-          <Nav.Link as={Link} to="/" className='text-muted' data-scroll-goto="4">About Us</Nav.Link>
-          <Nav.Link as={Link} to="/register" className='text-muted'>Register</Nav.Link>
-          <Nav.Link as={Link} to="/login" className='text-muted'>Login</Nav.Link>
+        <Nav.Link as={Link} to="/" className='text-muted' data-scroll-goto="2">Flowers</Nav.Link>
+        <Nav.Link as={Link} to="/" className='text-muted' data-scroll-goto="4">About Us</Nav.Link>
+        {!isAuthenticated
+          ? (
+            <Navbar className={styles.muted}>
+              <Nav.Link as={Link} to="/register" className='text-muted'>Register</Nav.Link>
+              <Nav.Link as={Link} to="/login" className='text-muted'>Login</Nav.Link>
+            </Navbar>
+          )
+          : (
+            <Navbar className={styles.muted}>
+              <Nav.Link as={Link} to="/create" className='text-muted'>Add Flower</Nav.Link>
+              <Nav.Link as={Link} to="/logout" className='text-muted'>Logout</Nav.Link>
+              <Navbar className="justify-content-end">
+                <Navbar.Text className={styles.signedIn}>
+                  Signed in as: <span className={`${styles.user}`}>{email}</span>
+                </Navbar.Text>
+              </Navbar>
+            </Navbar>
+          )
+        }
 
-          {/* Loged in */}
-          <Nav.Link as={Link} to="/create" className='text-muted'>Add Flower</Nav.Link>
-          {/* <Nav.Link as={Link} to="#link" className='text-muted'>Logout</Nav.Link> */}
-        </Navbar>
-        <Navbar className="justify-content-end">
-          <Navbar.Text className={styles.signedIn}>
-            Signed in as: <Link to="#login" className={`${styles.user}`}>Mark Otto</Link>
-          </Navbar.Text>
-        </Navbar>
+
       </Container>
     </Navbar>
   );
